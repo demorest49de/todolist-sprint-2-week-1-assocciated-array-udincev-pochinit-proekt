d1a9076f5b8d355ce57b5a73c90a05432e16c2e0
Shevchenko Andrey <demorest49de@gmail.com>
2024-02-29 23:10:55 +0300

    Changes Unversioned Files ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/.gitignore ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/App.css ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/App.tsx ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/public/favicon.ico ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/index.css ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/public/index.html ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/index.tsx ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/logo.svg ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/public/logo192.png ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/public/logo512.png ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/public/manifest.json ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/package.json ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/react-app-env.d.ts ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/README.md ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/public/robots.txt ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/serviceWorker.ts ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/setupTests.ts ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/src/Todolist.tsx ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/tsconfig.json ~/it-incubator/todolist - Спринт 02, неделя 1 - (ассоциативный массив) - Игорь Юдинцев-2/yarn.lock

---
+++ src/App.tsx
@@ -0,0 +1,101 @@
+import React, {useState} from 'react';
+import './App.css';
+import {Todolist} from './Todolist';
+import {v1} from 'uuid';
+
+export type FilterValuesType = "all" | "active" | "completed";
+
+type TodolistsType = {
+    id: string
+    title: string
+    filter: FilterValuesType
+}
+
+function App() {
+
+    let todolistID1=v1();
+    let todolistID2=v1();
+
+    let [todolists, setTodolists] = useState<Array<TodolistsType>>([
+        {id: todolistID1, title: 'What to learn', filter: 'all'},
+        {id: todolistID2, title: 'What to buy', filter: 'all'},
+    ])
+
+    let [tasks, setTasks] = useState({
+        [todolistID1]:[
+            {id: v1(), title: "HTML&CSS", isDone: true},
+            {id: v1(), title: "JS", isDone: true},
+            {id: v1(), title: "ReactJS", isDone: false},
+            {id: v1(), title: "Rest API", isDone: false},
+            {id: v1(), title: "GraphQL", isDone: false},
+        ],
+        [todolistID2]:[
+            {id: v1(), title: "HTML&CSS2", isDone: true},
+            {id: v1(), title: "JS2", isDone: true},
+            {id: v1(), title: "ReactJS2", isDone: false},
+            {id: v1(), title: "Rest API2", isDone: false},
+            {id: v1(), title: "GraphQL2", isDone: false},
+        ]
+    });
+
+    // let [filter, setFilter] = useState<FilterValuesType>("all");
+    function removeTask(id: string) {
+        let filteredTasks = tasks.filter(t => t.id != id);
+        setTasks(filteredTasks);
+    }
+
+    function addTask(title: string) {
+        let task = {id: v1(), title: title, isDone: false};
+        let newTasks = [task, ...tasks];
+        setTasks(newTasks);
+    }
+
+    function changeStatus(taskId: string, isDone: boolean) {
+        let task = tasks.find(t => t.id === taskId);
+        if (task) {
+            task.isDone = isDone;
+        }
+
+        setTasks([...tasks]);
+    }
+
+    console.log(todolists)
+    console.log(...todolists)
+    function changeFilter(todolistId: string, value: FilterValuesType) {
+      //  const result=[...todolists,todolists.map(el=>el.id===todolistId ? {...el,el.filter=value}  :el)]
+        //                                                                {id: v1(), title: 'What to buy', filter:value},
+     setTodolists(todolists.map(el=>el.id===todolistId ? {...el,filter:value,title: 'What to learn'} :el))
+
+    }
+
+    return (
+        <div className="App">
+            {todolists.map(el => {
+                let tasksForTodolist = tasks;
+                if (el.filter === "active") {
+                    tasksForTodolist = tasks.filter(t => !t.isDone);
+                }
+                if (el.filter === "completed") {
+                    tasksForTodolist = tasks.filter(t => t.isDone);
+                }
+                return (
+                    <Todolist
+                        key={el.id}
+                        todolistId={el.id}
+                        title={el.title}
+                        tasks={tasksForTodolist}
+                        removeTask={removeTask}
+                        changeFilter={changeFilter}
+                        addTask={addTask}
+                        changeTaskStatus={changeStatus}
+                        filter={el.filter}
+                    />
+                )
+            })}
+
+
+        </div>
+    );
+}
+
+export default App;
