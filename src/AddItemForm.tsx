bded422bd99a802d163cece7603a4efe3caaf0d0
Shevchenko Andrey <demorest49de@gmail.com>
2024-03-05 18:48:49 +0300

    Changes AddItemForm.tsx Todolist.tsx

--- src/AddItemForm.tsx
+++ src/AddItemForm.tsx
@@ -1,4 +1,5 @@
 import React, {ChangeEvent, KeyboardEvent, useState} from 'react';
+import {Button} from "@mui/material";
 
 type AddItemFormPropsType = {
     addItem: (title: string) => void
@@ -35,8 +36,7 @@ export function AddItemForm(props: AddItemFormPropsType) {
                onKeyPress={onKeyPressHandler}
                className={error ? "error" : ""}
         />
-        <button onClick={addItem}>+</button>
-
+        <Button variant={'contained'} color={'primary'} onClick={addItem}>+</Button>
         {error && <div className="error-message">{error}</div>}
     </div>
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -2,6 +2,8 @@ import React, {ChangeEvent} from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
+import {IconButton} from "@mui/material";
+import {Delete} from "@mui/icons-material";
 
 export type TaskType = {
     id: string
@@ -40,8 +42,8 @@ export function Todolist(props: PropsType) {
     const onCompletedClickHandler = () => props.changeFilter("completed", props.id);
 
     return <div>
-        <h3> <EditableSpan value={props.title} onChange={changeTodolistTitle} />
-            <button onClick={removeTodolist}>x</button>
+        <h3><EditableSpan value={props.title} onChange={changeTodolistTitle}/>
+            <IconButton onClick={removeTodolist}><Delete/></IconButton>
         </h3>
         <AddItemForm addItem={addTask}/>
         <ul>
@@ -59,8 +61,8 @@ export function Todolist(props: PropsType) {
 
                     return <li key={t.id} className={t.isDone ? "is-done" : ""}>
                         <input type="checkbox" onChange={onChangeHandler} checked={t.isDone}/>
-                        <EditableSpan value={t.title} onChange={onTitleChangeHandler} />
-                        <button onClick={onClickHandler}>x</button>
+                        <EditableSpan value={t.title} onChange={onTitleChangeHandler}/>
+                        <IconButton onClick={onClickHandler}><Delete/></IconButton>
                     </li>
                 })
             }
